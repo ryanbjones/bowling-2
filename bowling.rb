5913a1a183bea21c73f6d25114a3e98e9b6abbe3
Ryan Jones <run0391@gmail.com>
2015-01-28 14:34:31 -0700

    rewrote #roll method for bowling

--- bowling.rb
+++ bowling.rb
@@ -6,6 +6,16 @@ class Bowling
   end
 
   def roll(pins)
+    # add pins to current frame if incomplete
+    if @frames[@current_frame].incomplete?
+      @frames[@current_frame].add_roll(pins)
+    #move to next frame
+    else
+      @current_frame += 1
+      @frames[@current_frame].add_roll(pins)
+    end
+    @frames[prior_frame].add_bonus(pins) if @frames[prior_frame].needs_bonus?
+  end
 class Frame
   def initialize
     @rolls = []
