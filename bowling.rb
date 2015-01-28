385dd8c791fa475acb23189fc3807d083a2aa38a
Ryan Jones <run0391@gmail.com>
2015-01-28 14:42:36 -0700

    methods to describe status of frame and add bonus

--- bowling.rb
+++ bowling.rb
@@ -40,7 +40,25 @@ class Frame
     @score += pins
     @rolls.push(pins)
   end
+
+  def needs_bonus?
+    @bonus.length < 1 && spare?
+  end
+
+  def spare?
+    @rolls.reduce(:+) == 10 && @rolls.length == 2
+  end
+
+  def add_bonus(pins)
+    @bonus.push(pins)
     @score += pins
   end
 
+  def complete?
+    @rolls.length == 2 || @rolls.first == 10
+  end
+
+  def incomplete?
+    !complete?
+  end
 end
